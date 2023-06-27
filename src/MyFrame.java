ae53963a02bdf115c1c51c478d51b5bead601492
Stalino A J <93860427+Stalinosmj@users.noreply.github.com>
2023-06-27 21:00:43 +0530

    Update MyFrame.java

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -6,34 +6,35 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MyFrame extends JFrame {
 
     SimpleDateFormat timeFormat;
     SimpleDateFormat dayFormat;
     SimpleDateFormat dateFormat;
-    SimpleDateFormat timelineFormat;
 
     JLabel timeLabel;
     JLabel dayLabel;
     JLabel dateLabel;
-    JLabel timelineLabel;
 
-    String currentTime, Day, Date, timeline;
+    String currentTime, Day, Date;
 
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getContentPane().setBackground(Color.black);
         this.setTitle("Clock");
         this.setLayout(new FlowLayout());
-        this.setMinimumSize(new Dimension(530, 200));
+        this.setMinimumSize(new Dimension(600, 200));
         this.setResizable(true);
         this.setTitle("Clock");
 
         timeFormat = new SimpleDateFormat("     hh:mm:ss a    ");
         dayFormat = new SimpleDateFormat("   EEEE   ");
         dateFormat = new SimpleDateFormat("   MMMMM dd, yyyy   ");
-        timelineFormat = new SimpleDateFormat("  zzzz  ");
 
         timeLabel = new JLabel();
         timeLabel.setFont(new java.awt.Font("Verdana", Font.BOLD, 50));
@@ -65,36 +66,55 @@ public class MyFrame extends JFrame {
         dateLabel.setOpaque(true);
         this.add(dateLabel);
 
-        timelineLabel = new JLabel();
-        timelineLabel.setFont(new java.awt.Font("Helvetica", Font.PLAIN, 20));
-        timelineLabel.setForeground(new java.awt.Color(144, 238, 144));
-        timelineLabel.setBackground(new java.awt.Color(2, 48, 32));
-        timelineLabel.setHorizontalAlignment(JLabel.RIGHT);
-        timelineLabel.setVerticalAlignment(JLabel.BOTTOM);
-        timelineLabel.setOpaque(true);
-        this.add(timelineLabel);
+        String[] timezones = TimeZone.getAvailableIDs();
+        JComboBox<String> timezoneComboBox = new JComboBox<>(timezones);
+        timezoneComboBox.setFont(new java.awt.Font("Helvetica", Font.PLAIN, 20));
+        timezoneComboBox.setForeground(new java.awt.Color(144, 238, 144));
+        timezoneComboBox.setBackground(new java.awt.Color(2, 48, 32));
 
-        this.setVisible(true);
-        setTime();
+        timezoneComboBox.addActionListener
+        (new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String selectedTimezone = (String) timezoneComboBox.getSelectedItem();
+        TimeZone timeZone = TimeZone.getTimeZone(selectedTimezone);
+        Calendar calendar = Calendar.getInstance(timeZone);
+
+        String currentTime = timeFormat.format(calendar.getTime());
+        timeLabel.setText(currentTime);
+
+        String day = dayFormat.format(calendar.getTime());
+        dayLabel.setText(day);
+
+        String date = dateFormat.format(calendar.getTime());
+        dateLabel.setText(date);
     }
+});
 
-    public void setTime() {
-        while (true) {
+this.add(timezoneComboBox);
 
-            currentTime = timeFormat.format(Calendar.getInstance().getTime());
-            timeLabel.setText(currentTime);
-            Day = dayFormat.format(Calendar.getInstance().getTime());
-            dayLabel.setText(Day);
-            Date = dateFormat.format(Calendar.getInstance().getTime());
-            dateLabel.setText(Date);
-            timeline = timelineFormat.format(Calendar.getInstance().getTime());
-            timelineLabel.setText(timeline);
+this.setVisible(true);
+setTime();
+}
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+public void setTime() {
+while (true) {
+    Calendar calendar = Calendar.getInstance();
+
+    String currentTime = timeFormat.format(calendar.getTime());
+    timeLabel.setText(currentTime);
+
+    String day = dayFormat.format(calendar.getTime());
+    dayLabel.setText(day);
+
+    String date = dateFormat.format(calendar.getTime());
+    dateLabel.setText(date);
+
+    try {
+        Thread.sleep(1000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
     }
+}
+}
 }
